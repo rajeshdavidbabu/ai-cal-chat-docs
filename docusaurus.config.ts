bade6d711b2006c28e7aa3e012ca5e936251b423
Raj <rajeshdavid.b@gmail.com>
2024-05-26 13:20:33 +0200

    Initial commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,65 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const organizationName = "rajeshdavidbabu";
+const projectName = "ai-cal-chat-docs";
+
+const config: Config = {
+  title: "AI Cal Chat Docs",
+  tagline: "AI is cool",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: `https://${organizationName}.github.io`,
+  baseUrl: `/${projectName}/`,
+  trailingSlash: false,
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName, // Usually your GitHub org/user name.
+  projectName, // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.ts",
+        },
+        blog: false,
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: "AI Cal Chat",
+      logo: {
+        alt: "AI Cal Chat Docs Logo",
+        src: "img/logo-med.svg",
+      },
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
